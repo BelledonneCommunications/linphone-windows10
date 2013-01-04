7adb70e7ecbc3f17d3cde54274364168eda87ed7
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2013-01-04 14:01:04 +0100

    Use same contacts display as system + reworked interface to use WP app style

---
+++ Linphone/Model/ContactPictureConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Phone;
+using Microsoft.Phone.UserData;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linphone.Model
+{
+    public class ContactPictureConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Contact c = value as Contact;
+            if (c == null)
+                return null;
+
+            Stream imgStream = c.GetPicture();
+            if (imgStream != null)
+                return PictureDecoder.DecodeJpeg(imgStream);
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
