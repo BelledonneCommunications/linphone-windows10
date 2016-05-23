34798c6a3ef1cfd15d0e03f34e23586994bb5145
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2016-05-23 11:43:46 +0200

    Remove deprecated Visual Studio solutions.

--- submodules/build/xml2/libxml2_port.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef LIBXML2_PORT_H
-#define LIBXML2_PORT_H
-
-#define GetVersionEx(osvi)		(((osvi)->dwPlatformId = 0) != 0)
-#define getcwd(a, b) NULL
-#define getenv(x) NULL
-
-#endif /* LIBXML2_PORT_H */
