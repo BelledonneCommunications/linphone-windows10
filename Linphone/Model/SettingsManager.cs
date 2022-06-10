16a3aa10c696f8a11d7b6bc9ee0c7ac10fbcefe9
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2022-06-10 16:33:08 +0430

    Add pclappconfig. (see #6)

--- Linphone/Model/SettingsManager.cs
+++ Linphone/Model/SettingsManager.cs
@@ -15,6 +15,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 using Linphone;
+using PCLAppConfig;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
